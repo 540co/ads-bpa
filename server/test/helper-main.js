6aedaa292ebb59ff080c725e63e3777879de8f1a
Trey White <trey@2codemonkeys.com>
2015-06-20 16:35:11 -0400

    Updating helper-main.js with changes made in main.js

--- server/test/helper-main.js
+++ server/test/helper-main.js
@@ -1,45 +1,48 @@
+/*
+function mockDefinition(def, source, created_at, created_by) {
+  var definition = new Definition();
+
+  definition.definition = def;
+  definition.source = source;
+  definition.created_at = created_at;
+  definition.created_by = created_by;
+  definition.votes = mockVotes(
+    Math.floor(Math.random()*10),
+    Math.floor(Math.random()*10));
+
+  return definition;
+}
+
+
 // This function creates a mocked reactions instance for testing purposes
 function mockReaction() {
-/*  var reaction = new Reaction();
+  var reaction = new Reaction();
 
   reaction.reaction = "Death";
   reaction.definitions = new Array();
-  definitions.pop(mockDefinition(
+  reaction.definitions.pop(mockDefinition(
                     "Test Definition for Death",
                     "Merriam-Webster Medical Dictionary",
                     "2015-06-20 11:14:55",
                     "dre_app"));
 
-  return reaction;*/
+  return reaction;
 }
 
-
-function mockDefinition(definition, source, created_at, created_by) {
-/*  var definition = new Definition();
-
-  definition.definition = definition;
-  definition.source = source;
-  definition.created_at = created_at;
-  definition.created_by = created_by;
-  definition.votes(
-    Math.floor(Math.random()*10),
-    Math.floor(Math.random()*10));
-
-  return definition;*/
-}
 
 // This function creates a mocked votes instance for testing purposes with
 // supplied numbers of yeses and noes to instantiate with
 function mockVotes(yesVotes, noVotes) {
-/*  var votes = new Votes();
+  var votes = new Votes();
 
   for(var i=0; i<yesVotes; i++) {
     votes.up();
   }
 
   for(i=0; i<noVotes; i++) {
-    votes.down();
+    //votes.down();
   }
 
-  return votes;*/
+  return votes;
 }
+*/
