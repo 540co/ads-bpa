84975e1ddd8547abc5aa62145b48ed1539d21c00
John O'Brien <jobrieniii@540.co>
2015-06-20 06:18:09 -0400

    stubbed endpoints added, see README in /server folder for details

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,17 @@ var config = require('../config');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// TO DO: apis.json file
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({todo: 'apis.json file'});
 });
 
+// TO DO: swagger file
+router.get('/swagger', function(req, res, next) {
+  res.json({todo: 'swagger file'});
+});
+
+// Test Mongo Connection
 router.get('/mongodb', function(req, res, next) {
 
   console.log(config);
