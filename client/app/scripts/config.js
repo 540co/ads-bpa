e76aec7260b0b9daa938c471f50d3b58d483ba4a
Jeremy Lundstrom <lundstjb@gmail.com>
2015-06-20 23:51:20 -0400

    Adding config file

---
+++ client/app/scripts/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dreApp.constant:config
+ * @description
+ * # config
+ * config file of the dreApp
+ */
+angular.module('dreApp')
+  .constant('config', {
+    'apiKey': '<api key from open.fda.gov>'
+  });
