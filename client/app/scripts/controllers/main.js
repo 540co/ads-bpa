a914ee87ede14e7d05999f57c9db0db9b156baef
Jeremy Lundstrom <lundstjb@gmail.com>
2015-06-21 17:44:33 -0400

    Added more karma tests

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -11,10 +11,7 @@ angular.module('dreApp')
   .controller('MainCtrl', ['$scope', 'DashboardService', function ($scope, DashboardService) {
 
     $scope.initDashboard = function() {
-      // openFDA.adverseEvents.topSymptoms().then(function (data) {
-      //   $scope.allSymptoms = data.data.results;
-      //   $scope.allSymptoms = DashboardService.symptoms
-      // });
+
       DashboardService.getSymptoms().then(function (symptoms) {
         $scope.allSymptoms = symptoms;
       });
@@ -43,44 +40,37 @@ angular.module('dreApp')
         $scope.allEvents = events;
       });
 
-      // openFDA.adverseEvents.topManufacturers().then(function (data) {
-      //     $scope.allManufacturers = data.data.results;
-      // });
-      //
-      // openFDA.adverseEvents.topBrandNames().then(function (data) {
-      //     $scope.allBrands = data.data.results;
-      // });
-      //
-      // openFDA.adverseEvents.severityCount().then(function (data) {
-      //     $scope.allSeverityCount = data.data.results;
-      // });
-      //
-      // openFDA.adverseEvents.genderCount().then(function (data) {
-      //     $scope.allGenderCount = data.data.results;
-      // });
-      //
-      // openFDA.adverseEvents.topCountries().then(function (data) {
-      //   $scope.allCountries = data.data.results;
-      // });
     };
 
     $scope.getResults = function(keyword) {
-      //alert(keyword);
-      $scope.showResults = true;
-      // openFDA.adverseEvents.topSymptoms(keyword).then(function (data) {
-      //     $scope.ibuprofenSymptoms = data.data.results;
-      // });
-      // openFDA.adverseEvents.topManufacturers(keyword).then(function (data) {
-      //     $scope.ibuprofenManufacturers = data.data.results;
-      // });
-      // openFDA.adverseEvents.topBrandNames(keyword).then(function (data) {
-      //     $scope.ibuprofenBrands = data.data.results;
-      // });
-      // openFDA.adverseEvents.seriousCount(keyword).then(function (data) {
-      //     $scope.ibuprofenSeriousCount = data.data.results;
-      // });
-      // openFDA.adverseEvents.genderCount(keyword).then(function (data) {
-      //     $scope.ibuprofenSexCount = data.data.results;
-      // });
+
+      DashboardService.getSymptoms(keyword).then(function (symptoms) {
+        $scope.allSymptoms = symptoms;
+      });
+
+      DashboardService.getManufacturers(keyword).then(function (manufacturers){
+        $scope.allManufacturers = manufacturers;
+      });
+
+      DashboardService.getBrands(keyword).then(function (brands){
+        $scope.allBrands = brands;
+      });
+
+      DashboardService.getSeverity(keyword).then(function (severity){
+        $scope.allSeverityCount = severity;
+      });
+
+      DashboardService.getGenders(keyword).then(function (genders) {
+        $scope.allGenderCount = genders;
+      });
+
+      DashboardService.getCountries(keyword).then(function (countries) {
+        $scope.allCountries = countries;
+      });
+
+      DashboardService.getEvents(keyword).then(function (events) {
+        $scope.allEvents = events;
+      });
+      
     };
   }]);
--- client/test/spec/services/openFDA.js
+++ client/test/spec/services/openFDA.js
@@ -1,55 +1,87 @@
 'use strict';
 
-describe('Service: openFDA', function () {
+describe('Service : openFDA', function () {
 
   // load the controller's module
   beforeEach(module('dreApp'));
 
   var openFDA,
-    httpBackend;
+    $httpBackend;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function (_openFDA_, $httpBackend) {
+  // Initialize the service and a mock backend
+  beforeEach(inject(function ($httpBackend, _openFDA_) {
     openFDA = _openFDA_;
-    httpBackend = $httpBackend;
+    $httpBackend = $httpBackend;
   }));
 
-  it('should return no more than 25 symptoms', function () {
-    // openFDA.adverseEvents.topSymptoms().then(function(symptoms) {
-    //   expect(symptoms).toBeLessThan(26);
-    // });
+  describe('Method: Get Top Symptoms from openFDA', function() {
+    it('openFDA.adverseEvents.topSymptoms() should be a function', function() {
+      expect(angular.isFunction(openFDA.adverseEvents.topSymptoms)).toBe(true);
+    });
+
+    it('should return array of items', function () {
+      expect(openFDA.adverseEvents.topSymptoms()).toBeDefined();
+    });
   });
 
-  it('should return no more than 25 manufacturers', function () {
-    // openFDA.adverseEvents.topManufacturers().then(function(manufacturers) {
-    //   expect(manufacturers).toBeLessThan(26);
-    // });
+  describe('Method: Get Top Manufacturers from openFDA', function() {
+    it('openFDA.adverseEvents.topManufacturers() should be a function', function() {
+      expect(angular.isFunction(openFDA.adverseEvents.topManufacturers)).toBe(true);
+    });
+
+    it('should return array of items', function () {
+      expect(openFDA.adverseEvents.topManufacturers()).toBeDefined();
+    });
+});
+
+describe('Method: Get Top Brands from openFDA', function() {
+  it('openFDA.adverseEvents.topBrandNames() should be a function', function() {
+    expect(angular.isFunction(openFDA.adverseEvents.topBrandNames)).toBe(true);
   });
 
-  it('should return no more than 25 brands', function () {
-    // openFDA.adverseEvents.topBrandNames().then(function(brands) {
-    //   expect(brands).toBeLessThan(26);
-    // });
+  it('should return array of items', function () {
+    expect(openFDA.adverseEvents.topBrandNames()).toBeDefined();
+  });
+});
+
+describe('Method: Get Severity Count from openFDA', function() {
+  it('openFDA.adverseEvents.severityCount() should be a function', function() {
+    expect(angular.isFunction(openFDA.adverseEvents.severityCount)).toBe(true);
   });
 
-  it('should return no more than 25 countries', function () {
-    // openFDA.adverseEvents.topCountries().then(function(countries) {
-    //   expect(countries).toBeLessThan(26);
-    // });
+  it('should return array of items', function () {
+    expect(openFDA.adverseEvents.severityCount()).toBeDefined();
+  });
+});
+
+describe('Method: Get Gender Count from openFDA', function() {
+  it('openFDA.adverseEvents.genderCount() should be a function', function() {
+    expect(angular.isFunction(openFDA.adverseEvents.genderCount)).toBe(true);
   });
 
-  it('should return no more than 2 severities', function () {
-    //results - serious, non-serious
-    // openFDA.adverseEvents.severityCount().then(function(severity) {
-    //   expect(severity).toBeLessThan(3);
-    // });
+  it('should return array of items', function () {
+    expect(openFDA.adverseEvents.genderCount()).toBeDefined();
+  });
+});
+
+describe('Method: Get Top Countries from openFDA', function() {
+  it('openFDA.adverseEvents.topCountries() should be a function', function() {
+    expect(angular.isFunction(openFDA.adverseEvents.topCountries)).toBe(true);
   });
 
-  it('should return no more than 25 countries', function () {
-    //results - male, female, unknown
-    // openFDA.adverseEvents.genderCount().then(function(genders) {
-    //   expect(genders).toBeLessThan(4);
-    // });
+  it('should return array of items', function () {
+    expect(openFDA.adverseEvents.topCountries()).toBeDefined();
   });
+});
+
+describe('Method: Get Event Count by Date from openFDA', function() {
+  it('openFDA.adverseEvents.eventCountByDate() should be a function', function() {
+    expect(angular.isFunction(openFDA.adverseEvents.eventCountByDate)).toBe(true);
+  });
+
+  it('should return array of items', function () {
+    expect(openFDA.adverseEvents.eventCountByDate()).toBeDefined();
+  });
+});
 
 });
