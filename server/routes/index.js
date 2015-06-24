ef56c348266f11316b82338ad707fed87d024228
John O'Brien <jobrieniii@540.co>
2015-06-24 02:58:56 -0400

    added jade templates to render contents of APIs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 var config = require('../config');
 var fs = require('fs');
 var express = require('express');
+var jade = require('jade');
+var request = require('request');
 var router = express.Router();
 
 // TO DO: apis.json file
@@ -14,6 +16,45 @@ router.get('/swagger', function(req, res, next) {
   swagger.host = config.host;
   swagger.schemes = config.schemes;
   res.json(swagger);
+});
+
+// View reaction definitions / votes currently stored in database
+router.get('/admin', function(req, res, next) {
+
+  res.render('admin', config);
+
+});
+
+// View reaction definitions / votes currently stored in database
+router.get('/admin/reactions', function(req, res, next) {
+
+  var url = config.schemes[0] + '://' + config.host + '/api/reactions?limit=10000';
+
+  request(url, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      res.render('admin-reactions', JSON.parse(body));
+
+    }
+
+  });
+
+});
+
+// View top searches (admin view)
+router.get('/admin/searches', function(req, res, next) {
+
+  var url = config.schemes[0] + '://' + config.host + '/api/searches?limit=500';
+
+  request(url, function (error, response, body) {
+
+    if (!error && response.statusCode === 200) {
+      res.render('admin-searches', JSON.parse(body));
+
+    }
+
+  });
+
 });
 
 
