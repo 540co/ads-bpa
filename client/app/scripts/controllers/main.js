7f346c90b4a1de9e082dc4484bea3abb0daa88b3
Patrick Knowlan <patrick@540.co>
2015-06-24 22:45:18 -0400

    missing param
    missing param for the view to update for the selected filter values

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -16,15 +16,20 @@ angular.module('dreApp')
     };
 
     $scope.getResults = function(keyword, filter) {
+      var filterList = refreshFilters(filter);
       $scope.showFilter = true;
       $scope.searchTerm = keyword;
-      setDashboard(keyword);
+      setDashboard(keyword, filterList);
       $scope.definitions = [];
     };
 
+    function setDashboard(keyword, filterList) {
 
+      loadSymptoms(keyword, filterList)
         .then(parallelLoad);
 
+      DashboardService.getSymptoms(keyword, filterList)
+      .then(function(symptoms) {
         $scope.allSymptoms = symptoms;
         _.forEach(symptoms, function(symptom) {
           return DashboardService.getSymptomDefinitions(symptom.term);
@@ -33,7 +38,11 @@ angular.module('dreApp')
         $scope.definition = definition;
       });
 
+      DashboardService.getManufacturers(keyword, filterList).then(function(manufacturers) {
         $scope.allManufacturers = manufacturers;
+        var manufacturerNames = [],
+          manufacturerCounts = [];
+        _.forEach($scope.allManufacturers, function(manufacturer) {
           manufacturerNames.push(manufacturer.term);
           manufacturerCounts.push(manufacturer.count);
         });
@@ -41,19 +50,27 @@ angular.module('dreApp')
         $scope.manufacturerNames = manufacturerNames;
       });
 
+      DashboardService.getBrands(keyword, filterList).then(function(brands) {
         $scope.allBrands = brands;
       });
 
+      DashboardService.getSeverity(keyword, filterList).then(function(severity) {
         $scope.allSeverityCount = severity;
       });
 
+      DashboardService.getGenders(keyword, filterList).then(function(genders) {
         $scope.allGenderCount = genders;
       });
 
+      DashboardService.getCountries(keyword, filterList).then(function(countries) {
         $scope.allCountries = countries;
       });
 
+      DashboardService.getEvents(keyword, filterList).then(function(events) {
         $scope.allEvents = events;
+        var eventDates = [],
+          eventCounts = [];
+        _.forEach($scope.allEvents, function(event) {
           eventDates.push(parseDate(event.time));
           eventCounts.push(event.count);
         });
@@ -65,11 +82,58 @@ angular.module('dreApp')
     }
 
     function parseDate(str) {
+      var y = str.substr(0, 4),
+        m = str.substr(4, 2) - 1,
+        d = str.substr(6, 2);
+      var D = new Date(y, m, d);
       return (D.getFullYear() == y && D.getMonth() == m && D.getDate() == d) ? m.toString() + '/' + d + '/' + y : 'invalid date';
     }
 
+    var loadSymptoms = function(drugKeyword, filterList) {
+        return DashboardService.getSymptoms(drugKeyword, filterList);
+      },
+      parallelLoad = function(symptoms) {
+        var definition = DashboardService.getSymptomDefinitions('death');
+        var allDefs = [];
 
+        _.forEach(symptoms, function(symptom) {
+          var defCall = DashboardService.getSymptomDefinitions(symptom.term);
+          allDefs.push(defCall);
+        });
 
+        return $q.all(_.slice(allDefs, 0, 8)).then(function(data) {
+          $scope.definitions = data;
 
+        });
+      };
+
+    //Checkbox Filter START
+
+    $scope.filterModel = [{
+      "name": "ots",
+      "type": "product_type",
+      "term": "HUMAN OTC DRUG",
+      "title": "Over the Counter",
+      "value": false
+    }, {
+      "name": "otc",
+      "type": "product_type",
+      "term": "HUMAN PRESCRIPTION DRUG",
+      "title": "Off the Shelf",
+      "value": false
+    }];
+
+    function refreshFilters(item) {
+      var filterItems = [];
+      _.each(item, function(n) {
+        var object = {};
+        if (n.value == true) {
+          object.filter = n.type;
+          object.filterText = n.term;
+          filterItems.push(object);
+        } else {}
       });
+      return filterItems;
+    }
+
   }]);
