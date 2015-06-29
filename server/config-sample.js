44a80b40c80fa88ca0cff5c86e8c4d0403781cb0
John O'Brien <jobrieniii@540.co>
2015-06-29 12:02:43 -0400

    moved collection names for searches and reactions to config file

--- server/config-sample.js
+++ server/config-sample.js
@@ -5,6 +5,9 @@ config.host = "localhost:3000";
 
 config.mongo = "mongodb://localhost:27017/";
 config.db = "MONGO DB NAME HERE";
+config.reactions_collection = "COLLECTION_FOR_REACTIONS";
+config.searches_collection = "COLLECTION_FOR_SEARCHES";
+
 config.dictionaryapi_key = 'API KEY HERE';
 config.wordnikapi_key = 'API KEY HERE';
 
