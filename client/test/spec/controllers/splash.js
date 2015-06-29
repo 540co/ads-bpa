24f796396087f38191b6d9aa3988faf41aca92ba
Jeremy Lundstrom <lundstjb@gmail.com>
2015-06-29 13:01:11 -0400

    Beginning new tests

--- client/test/spec/controllers/splash.js
+++ client/test/spec/controllers/splash.js
@@ -6,32 +6,86 @@ describe('Controller: SplashCtrl', function () {
   beforeEach(module('dreApp'));
 
   var AboutCtrl,
-    scope;
+    $scope, service, $controller, setTerms;
+
+    var mockTerms = [
+      {
+        search: 'ibuprofen',
+        count: 199
+      },
+      {
+        search: 'motrin',
+        count: 384
+      },
+      {
+        search: 'aleve',
+        count: 11
+      },
+      {
+        search: 'xanax',
+        count: 1939
+      },
+    ];
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    AboutCtrl = $controller('SplashCtrl', {
-      $scope: scope
+  beforeEach(inject(function ($rootScope, $controller, $q, DashboardService) {
+    $scope = $rootScope.$new();
+    service = DashboardService;
+
+    //console.log(service);
+
+    $controller('SplashCtrl', {
+      $scope: $scope,
+      DashboardService: service
     });
+
+    //$rootScope.apply();
+
   }));
 
   describe('Method: init', function() {
 
-    beforeEach(function() {
-      scope.init();
-    });
+    beforeEach(inject (function($q) {
+      $scope.init();
+      var termsDeferred = $q.defer();
+      termsDeferred.resolve(mockTerms);
+
+      console.log(termsDeferred.promise);
+      spyOn(service, 'getSearchTerms').and.returnValue(termsDeferred.promise);
+      setTerms = service.getSearchTerms();
+    }));
 
     it('should have function named $scope.init()', function () {
-      expect(angular.isFunction(scope.init)).toBe(true);
+      expect(angular.isFunction($scope.init)).toBe(true);
+    });
+
+    it('should call getSearchTerms', function () {
+      expect(setTerms).toEqual([
+        {
+          search: 'ibuprofen',
+          count: 199
+        },
+        {
+          search: 'motrin',
+          count: 384
+        },
+        {
+          search: 'aleve',
+          count: 11
+        },
+        {
+          search: 'xanax',
+          count: 1939
+        },
+      ]);
     });
 
   });
-
+  //
   describe('Method: search', function() {
 
     it('should have function named $scope.search()', function () {
-      expect(angular.isFunction(scope.search)).toBe(true);
+      //expect(angular.isFunction($scope.search)).toBe(true);
     });
 
   });
