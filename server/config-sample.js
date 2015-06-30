dd4150068cb79e77da387f8ad46b45ed7916b4bb
John O'Brien <jobrieniii@540.co>
2015-06-30 08:02:55 -0400

    updated travis and config

--- server/config-sample.js
+++ server/config-sample.js
@@ -4,11 +4,11 @@ config.schemes = ['http'];
 config.host = "localhost:3000";
 
 config.mongo = "mongodb://localhost:27017/";
-config.db = "MONGO DB NAME HERE";
+config.db = "MONGO_DB_NAME_HERE";
 config.reactions_collection = "COLLECTION_FOR_REACTIONS";
 config.searches_collection = "COLLECTION_FOR_SEARCHES";
 
-config.dictionaryapi_key = 'API KEY HERE';
-config.wordnikapi_key = 'API KEY HERE';
+config.dictionaryapi_key = 'API_KEY_HERE';
+config.wordnikapi_key = 'API_KEY_HERE';
 
 module.exports = config;
