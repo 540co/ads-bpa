1d33819036e92fccd269feda2e242d282c71c70b
John O'Brien <jobrieniii@540.co>
2015-07-01 04:50:37 -0400

    updated swagger api docs to include model/schemas, updated swagger UI to have look / feel more similar to DRE app, added dictionary lookup links directly swagger docs, updated readme links

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 var express = require('express');
 var jade = require('jade');
 var request = require('request');
+var _ = require('lodash');
+var moment = require('moment');
 var router = express.Router();
 
 
@@ -25,7 +27,33 @@ router.get('/reactions', function(req, res, next) {
   request(url, function (error, response, body) {
 
     if (!error && response.statusCode === 200) {
-      res.render('admin-reactions', JSON.parse(body));
+      var body = JSON.parse(body);
+
+      var meta = body.meta;
+      var reactions = body.data;
+
+      _.forEach(reactions, function (reaction, reactionIndex) {
+
+        if(reaction.last_updated) {
+          reactions[reactionIndex].last_updated_english = moment(reaction.last_updated).fromNow();
+        } else {
+          reactions[reactionIndex].last_updated_english = "unknown / not set properly";
+        }
+
+        _.forEach(reaction.definitions, function (definition, definitionIndex) {
+
+          if(definition.created_at) {
+            reactions[reactionIndex].definitions[definitionIndex].created_at_english = moment(definition.created_at).fromNow();
+          } else {
+            reactions[reactionIndex].definitions[definitionIndex].created_at_english = "unknown / not set properly";
+          }
+
+        });
+
+      });
+
+
+      res.render('admin-reactions', {meta:meta, data: reactions});
 
     }
 
